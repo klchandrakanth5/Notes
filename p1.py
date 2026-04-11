6a8c106c646f8638956c29a1b06c075473cbcc8c
klmanu <klmanu5@gmail.com>
2026-04-11 14:02:08 +0530

    Done

--- p1.py
+++ p1.py
@@ -1,5 +1,5 @@
 import numpy as np
-arr = np.array([1,2,3,4,5])
-arr1 = np.array([6])
-a = arr + arr1
-print("Added value",a)
+a1 = np.array([1,2,3,4])
+a2 = np.array([7,8])
+a = np.all(a1 > 7)
+print("Content -->",a)
