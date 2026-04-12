68786d8e528a01ed02f8f9ace2f7140e0feded4f
klmanu <klmanu5@gmail.com>
2026-04-12 09:17:52 +0530

    Done

--- p1.py
+++ p1.py
@@ -1,10 +1,3 @@
-import numpy as np
-
-arr = np.array([
-    [[1, 2, 3], [4, 5, 6]],
-    [[7, 8, 9], [10, 11, 12]]
-])
-
-print(arr[0,0:1,0:1])
-print(arr[0:1,1:2,0:1])
-print(arr[1,0:1,0:1])
+num1 = int(input("Enter the first number :\n"))
+num2 = int(input("Enter the second number :\n"))
+print(num1 > num2)
