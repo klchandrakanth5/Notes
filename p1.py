e10369cfd2c13ba7ee5c63575354a677df5a4e49
klmanu <klmanu5@gmail.com>
2026-04-20 10:13:47 +0530

    Done

--- p1.py
+++ p1.py
@@ -1,4 +1,8 @@
-num1 = 12
-num2 = 78
-print(num1 + num2)
-print(num1 - num2)
+num1 = int(input("Enter the number :\n"))
+num2 = int(input("Enter the second :\n"))
+if num1 > num2:
+    print(num1)
+elif num2>num1:
+    print(num2)
+else:
+    pass
