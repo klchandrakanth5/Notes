0419b8b969de6311d93fe51a6a497f871b844ad8
klmanu <klmanu5@gmail.com>
2026-04-30 12:14:59 +0530

    Done

--- p1.py
+++ p1.py
@@ -1,8 +1,1 @@
-num1 = int(input("Enter the number :\n"))
-num2 = int(input("Enter the second :\n"))
-if num1 > num2:
-    print(num1)
-elif num2>num1:
-    print(num2)
-else:
-    pass
+print("hello welcome")
